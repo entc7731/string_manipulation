d323432f72af449d98d1abb9f466ae62722888d3
suhelmulla <suhel19690@gmail.com>
2022-07-09 18:38:57 +0530

    Added strcpy() function code
    Added strcpy() function code

--- string_manipulation/string_file_func/ConsoleApplication1/ConsoleApplication1.cpp
+++ string_manipulation/string_file_func/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,18 +9,34 @@
 
 int strlen(char* s1)
 {
-    int count = 0;
+    int length = 0;
 
-    while (s1[count] != '\0')
-        count++;
+    while (s1[length] != '\0')
+        length++;
 
-    return count;
+    return length;
+}
+
+char* strcpy(char* s1, char* s2)
+{
+    int length = 0;
+    
+    while (s2[length] != '\0')
+        length++;
+
+    for (int i = 0; i < length; i++)
+        s1[i] = s2[i];
+
+    s2[length] = '\0';
+
+    return s2;
 }
 
 char* strrev(char *s1)
 {
     int length = 0;
     char temp;
+
     while (s1[length] != '\0')
         length++;
 
@@ -40,8 +56,10 @@ char* strrev(char *s1)
 int main()
 {
     char str1[50] = "Suhel Mulla";
+    char str2[50] = "";
 
     printf("\n Length = %d", strlen((char*)str1));
+    printf("\n Copied string = %s", strcpy(str2, str1));
     printf("\n Reversed string = %s", strrev((char *)str1));
 
     return 0;
