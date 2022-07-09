dc25f5efdfe17fe12a56793da12a34e7c0e82110
suhelmulla <suhel19690@gmail.com>
2022-07-09 11:41:31 +0530

    Added strlen() function
    Added strlen() function, note difference between (char *) and (const char *).

--- string_manipulation/string_file_func/ConsoleApplication1/ConsoleApplication1.cpp
+++ string_manipulation/string_file_func/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,31 @@
+
+#define USE_STRING_H_FILE 0
 #include <stdio.h>
+
+#if USE_STRING_H_FILE
+#include <string.h>
+
+#else
+
+int strlen(char* s1)
+{
+    int count = 0;
+
+    while (s1[count] != '\0')
+        count++;
+
+    return count;
+}
+
+#endif
+
 
 int main()
 {
+    int res = 0;
+    const char *str1 = "Suhel Mulla";
+    res = strlen((char *)str1);
+    printf("length = %d", res);
 
     return 0;
 }
